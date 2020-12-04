68932ae92604fc7b5c0abeacd71fa49b2f55ba8c
Bruno Buss <brunooliveira9895@gmail.com>
2020-12-04 16:40:19 +0000

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import "react-calendar-heatmap/dist/styles.css";
 
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Repo from "./pages/Repo";
 import Footer from "./components/Footer";
+import { ThemeName, themes } from "./styles/themes";
 
 function App() {
+  const [themeName, setThemeName] = useState<ThemeName>("light");
+  const currentTheme = themes[themeName];
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:repository" element={<Repo />} />
-      </Routes>
-      <Footer />
-      <GlobalStyles />
-    </BrowserRouter>
+    <ThemeProvider theme={currentTheme}>
+      <BrowserRouter>
+        <Header themeName={themeName} setThemeName={setThemeName} />
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/:username/:repository" element={<Repo />} />
+        </Routes>
+        <Footer />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
